f3868035380b76e12f5ad97fa45956ad3bb6a8f3
Dlol <dravennadas@gmail.com>
2022-06-03 16:06:03 -0500

    h

--- src/app.rs
+++ src/app.rs
@@ -2,20 +2,15 @@
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
-    value: f32,
+    value: i32,
+    amt: String
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            value: 0,
+            amt: String::from("0"),
         }
     }
 }
@@ -45,7 +40,7 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let Self { value, amt } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -63,49 +58,29 @@ impl eframe::App for TemplateApp {
             });
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("big penis");
+            if ui.button("+").clicked() {
+                let amt = amt.parse::<i32>();
+                let mut inc = 1;
+                match amt {
+                    std::result::Result::Ok(a) => inc = a,
+                    _ => {}
+                }
+                *value += inc;
+            }
+            ui.label(value.to_string());
+            if ui.button("-").clicked() {
+                let amt = amt.parse::<i32>();
+                let mut inc = 1;
+                match amt {
+                    std::result::Result::Ok(a) => inc = a,
+                    _ => {}
+                }
+                *value -= inc;
             }
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
-            });
+            ui.add(egui::TextEdit::singleline(amt).hint_text("Amount to add"))
         });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
-        });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
     }
 }
